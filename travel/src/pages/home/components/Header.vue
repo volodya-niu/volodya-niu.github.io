3b6d9991832a27f6ff170546f3f6f59b560a8818
volodya <18356090933@163.com>
2018-07-01 11:25:12 +0800

    phone

---
+++ travel/src/pages/home/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+            </div>
+      <router-link to="/city">
+        <div class="header-right">
+            {{this.city}}
+            <span class="icon-place"></span>
+             <span class="iconfont arrow-icon">&#xe62d;</span>
+            </div>
+       </router-link>           
+    </div>
+</template>
+<script>
+import {mapState} from 'vuex'
+    export default{
+        name:'HomeHeader',
+        computed:{
+            ...mapState(['city']),
+            //...mapGetters(['doubleCity'])
+        }
+    }
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+    .header
+            display: flex 
+            height :.86rem
+            background:$bgColor
+            color:#fff
+        
+    .header-left   
+            width: .64rem
+            float :left
+            line-height:.8rem
+    .back-icon 
+        text-align center
+        font-size .4rem
+    .header-input
+            flex: 1
+            height:.64rem
+            line-height .64rem
+            margin-top:.12rem
+            margin-left:.2rem
+            padding-left .2rem
+            background:#fff
+            border-radius:.1rem
+            color #000 
+    .header-right
+            min-width:1.04rem 
+            padding 0 .1rem 
+            float:right
+            line-height:.8rem
+            text-align:center
+            color #fff
+    .arrow-icon
+            font-size .24rem
+            margin-left -.04rem   
+</style>
